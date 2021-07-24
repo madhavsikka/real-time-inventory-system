9791d62bc99cabfcfb8cd6db3fd4906ba809a69e
Madhav Sikka <300072942@BLRM-300072942.local>
2021-07-24 16:21:57 +0530

    add database schema

--- src/main/java/com/github/madhav/SpringKafka/item_detail/ItemDetailRepository.java
+++ src/main/java/com/github/madhav/SpringKafka/item_detail/ItemDetailRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.Query;
 public interface ItemDetailRepository extends JpaRepository<ItemDetail, Long> {
     @Query(value = "SELECT * FROM item_detail WHERE fk_item_id = ?1 and fk_warehouse_id = ?2", nativeQuery = true)
     ItemDetail findByItemIdAndWarehouseId(Long itemId, Long warehouseId);
-    //    @Query(value = "UPDATE item_detail detail set detail.stock = ?3 WHERE detail.fk_item_id = ?1 and detail.fk_warehouse_id = ?2", nativeQuery = true)
 }
